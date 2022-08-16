fec0ee658dbdee284b07d32be5f607ef8e89af19
LAKSH DOSHI <102138784+LAKSH3002@users.noreply.github.com>
2022-08-16 16:53:52 +0530

    Add files via upload
    Program for global and local scope variables.

---
+++ globallocal.c
@@ -0,0 +1,39 @@
+//CODE 1 
+/*#include<stdio.h>
+int a=10;
+float b = 5.5;
+int product()
+{
+    int x=10;
+    float y=10.0;
+    int z = x*y;
+    int z1 = a*b;
+    printf("THE PRODUCT OF X AND Y IS = %d\n",z);
+     printf("THE PRODUCT OF A AND B IS = %d\n",z1);
+}
+int main()
+{
+    int c;
+    c=a+b;
+    printf("THE SUM OF A AND B IS = %d\n",c);
+    product();
+}*/
+
+/*#include<stdio.h>
+int x;
+float x;
+void subract()
+{
+    float a=10.8;
+    int a = 10;
+    int s = a-a;
+    printf("THE DIFFERENCE OF A AND A IS = %d",s);
+}
+int main()
+{
+    int p;
+    p = x*x;
+    printf("THE PRODUCT OF X AND X IS = %d",p);
+    subract();
+}*/
+
